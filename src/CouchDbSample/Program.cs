179dd76e4c5a15772fcc450e89d3e1b85e78a40f
Tiago de C. Resende <tiagor87@gmail.com>
2021-06-13 12:15:18 -0300

    :tada: Creates couchdbSample

---
+++ src/CouchDbSample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Unifeso.Db.CouchDbSample.Extensions;
+using Unifeso.Db.CouchDbSample.Infrastructure;
+
+namespace Unifeso.Db.CouchDbSample
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await using var context = new CouchDbContext();
+            // Create person
+            await context.CreatePersonAsync();
+            
+            // Read person
+            var people = await context.ReadPeopleAsync();
+            people.Print();
+            
+            // Update person
+            var person = people.First();
+            await context.UpdateAsync(person);
+            people = await context.ReadPeopleAsync();
+            people.Print();
+
+            // Delete person
+            await context.DeleteListAsync(people);
+            people = await context.ReadPeopleAsync();
+            people.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
